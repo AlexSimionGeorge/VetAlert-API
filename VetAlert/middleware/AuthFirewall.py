f8ae6b306653c25a97f5619a3d472d1e57fa7231
AlexSimionGeorge <alexsimiongeorge@gmail.com>
2024-12-30 23:15:15 +0200

    fixed time sinking problems for auth

--- VetAlert/middleware/AuthFirewall.py
+++ VetAlert/middleware/AuthFirewall.py
@@ -1,14 +1,10 @@
-from django.http import JsonResponse
 from firebase_admin import auth
+from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
-
 from api.models.FirebaseAuthUser import FirebaseAuthUser
 
 class AuthFirewall(MiddlewareMixin):
     def process_request(self, request):
-        """
-        This method is called on each request before the view (and other middleware).
-        """
         if not request.path.startswith('/api/'):
             return
 
@@ -21,7 +17,8 @@ class AuthFirewall(MiddlewareMixin):
 
         try:
             token = auth_header.split(" ")[1]
-            decoded_token = auth.verify_id_token(token)
+            # Allow for clock skew of up to 10 seconds
+            decoded_token = auth.verify_id_token(token, clock_skew_seconds=10)
 
             request.user = FirebaseAuthUser(
                 uid=decoded_token['uid'],
@@ -29,11 +26,6 @@ class AuthFirewall(MiddlewareMixin):
                 email=decoded_token.get('email'),
             )
         except Exception as e:
-            return JsonResponse({"error": "Unauthorized: Invalid token"}, status=401)
-
-
-    def process_response(self, request, response):
-        """
-        This method is called on each response before it is returned to the client.
-        """
-        return response
+            import traceback
+            print("Error verifying token:\n", traceback.format_exc())
+            return JsonResponse({"error": "Unauthorized: Invalid token", "details": str(e)}, status=401)
